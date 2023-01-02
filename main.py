91caae5651467374ca532ea4e4072199ab0cbf07
Raven <yakubsiq@gmail.com>
2023-01-02 18:02:21 +0100

    Created basic login
    There's no whitelist for now, so everybody can login

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 import json
 
+from datetime import timedelta
+
+
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=7)
+
 
 def get_meme():
     url = "https://meme-api.com/gimme/czechmemes"
@@ -12,9 +18,7 @@ def get_meme():
     return meme_large, subreddit
     
 
-@app.route("/base-not-permitted")
-def base():
-    return render_template("base.html")
+
 
 @app.route("/meme")
 def meme():
@@ -29,8 +33,27 @@ def home():
 @app.route("/login-page", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["usrnm"]
-        session['user'] = user
-    return render_template("login.html")
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 
 app.run(debug=True, host="0.0.0.0", port=80)
