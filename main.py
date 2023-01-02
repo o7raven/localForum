11914163ec7ee1d64bd088710819960325abdfa2
Raven <yakubsiq@gmail.com>
2023-01-02 19:49:31 +0100

    Created database for saving users
    But you are still able to create as many as users as you want

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import requests
 import json
+from flask_sqlalchemy import SQLAlchemy
 
 from datetime import timedelta
 
 
 app = Flask(__name__)
 app.secret_key = "hello"
-app.permanent_session_lifetime = timedelta(days=7)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+app.app_context().push()
 
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
 
 def get_meme():
     url = "https://meme-api.com/gimme/czechmemes"
@@ -28,7 +43,9 @@ def meme():
 @app.route("/")
 def home():
     return render_template("index.html")
-
+@app.route("/users")
+def view():
+    return render_template("users.html", values=users.query.all())
 
 @app.route("/login-page", methods=["POST", "GET"])
 def login():
@@ -36,24 +53,55 @@ def login():
         session.permanent = True
         user = request.form["usrnm"]
         session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
+
+        flash("Přihlášen!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email uložen!")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
     else:
+        flash("Nejsi přihlášen!")
         return redirect(url_for("login"))
     
 @app.route("/logout")
 def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"Úspěšně odhlášen!", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 
-app.run(debug=True, host="0.0.0.0", port=80)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True, host="0.0.0.0", port=80)
